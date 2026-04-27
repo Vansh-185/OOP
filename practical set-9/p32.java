78e9038ef4f445583cb8984d35b09d33088930f0
Vansh-185 <vanshamipara185@gmail.com>
2026-04-27 16:57:47 +0530

    Implement search functionality for roll numbers and names

--- practical set-9/p32.java
+++ practical set-9/p32.java
@@ -1,1 +1,42 @@
+import java.util.LinkedList;
+import java.util.Scanner;
 
+public class Main {
+
+    public static <T> boolean searchElement(LinkedList<T> list, T element) {
+        return list.contains(element);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        LinkedList<Integer> rollList = new LinkedList<>();
+        System.out.print("Enter number of roll numbers: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter roll number: ");
+            rollList.add(sc.nextInt());
+        }
+
+        System.out.print("Enter roll number to search: ");
+        int searchRoll = sc.nextInt();
+        System.out.println("Found: " + searchElement(rollList, searchRoll));
+
+        sc.nextLine();
+
+        LinkedList<String> nameList = new LinkedList<>();
+        System.out.print("Enter number of names: ");
+        int m = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < m; i++) {
+            System.out.print("Enter name: ");
+            nameList.add(sc.nextLine());
+        }
+
+        System.out.print("Enter name to search: ");
+        String searchName = sc.nextLine();
+        System.out.println("Found: " + searchElement(nameList, searchName));
+    }
+}
