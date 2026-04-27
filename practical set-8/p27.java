1bf688e0da4bc01b3d451b61c094d30877f15d5c
Vansh-185 <vanshamipara185@gmail.com>
2026-04-27 16:55:04 +0530

    Implement student record input and output

--- practical set-8/p27.java
+++ practical set-8/p27.java
@@ -1,1 +1,48 @@
+import java.io.*;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        FileWriter fw = null;
+        BufferedReader br = null;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            fw = new FileWriter("students.txt");
+
+            for (int i = 1; i <= 3; i++) {
+                System.out.print("Enter roll no: ");
+                int roll = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Enter name: ");
+                String name = sc.nextLine();
+
+                System.out.print("Enter marks: ");
+                int marks = sc.nextInt();
+
+                fw.write(roll + " " + name + " " + marks + "\n");
+            }
+
+            fw.close();
+
+            br = new BufferedReader(new FileReader("students.txt"));
+            String line;
+
+            System.out.println("\nStudent Records:");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("File error");
+        } finally {
+            try {
+                if (fw != null) fw.close();
+                if (br != null) br.close();
+            } catch (IOException e) {
+                System.out.println("Error closing file");
+            }
+        }
+    }
+}
