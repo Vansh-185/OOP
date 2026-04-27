48c8d5862a309f426f57112d69f2e5ce742ea17d
Vansh-185 <vanshamipara185@gmail.com>
2026-04-27 16:57:01 +0530

    Add word count functionality to main class
    This program counts the occurrences of each word in a given sentence and prints the results.

--- practical set-9/p30.java
+++ practical set-9/p30.java
@@ -1,1 +1,26 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String input = sc.nextLine();
+
+        String words[] = input.split("\\s+");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+
+        for (String key : map.keySet()) {
+            System.out.println(key + " -> " + map.get(key));
+        }
+    }
+}
