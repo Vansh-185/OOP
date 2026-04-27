83c57c765c578e279f503329e36a6de0107fe160
Vansh-185 <vanshamipara185@gmail.com>
2026-04-27 16:46:08 +0530

    Implement Employee and Manager classes with data methods

--- practical set-4/p15.java
+++ practical set-4/p15.java
@@ -1,1 +1,53 @@
+import java.util.Scanner;
 
+class Employee {
+    String name;
+    String department;
+
+    void readData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter employee name: ");
+        name = sc.nextLine();
+        System.out.print("Enter department: ");
+        department = sc.nextLine();
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+    }
+}
+
+class Manager extends Employee {
+    int teamSize;
+
+    void readManagerData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter manager name: ");
+        name = sc.nextLine();
+        System.out.print("Enter department: ");
+        department = sc.nextLine();
+        System.out.print("Enter team size: ");
+        teamSize = sc.nextInt();
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Team Size: " + teamSize);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        System.out.println("Employee Details:");
+        e.readData();
+        e.displayDetails();
+
+        Manager m = new Manager();
+        System.out.println("\nManager Details:");
+        m.readManagerData();
+        m.displayDetails();
+    }
+}
