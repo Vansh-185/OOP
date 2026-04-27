1a0feb4c01add18ecd903085902e11c3015a990e
Vansh-185 <vanshamipara185@gmail.com>
2026-04-27 16:47:03 +0530

    Implement division classification based on average marks

--- practical set-5/p16.java
+++ practical set-5/p16.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+interface Classify {
+    String getDivision(double average);
+}
+
+class Result implements Classify {
+    public String getDivision(double average) {
+        if (average >= 60)
+            return "First Division";
+        else
+            return "No First Division";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter average marks: ");
+        double avg = sc.nextDouble();
+
+        Result r = new Result();
+        System.out.println(r.getDivision(avg));
+    }
+}
