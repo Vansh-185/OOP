90ec2cd83fefebfc7361bc19c42529e55b64af62
Vansh-185 <vanshamipara185@gmail.com>
2026-04-27 16:56:26 +0530

    Add functionality to input and display marks

--- practical set-9/p29.java
+++ practical set-9/p29.java
@@ -1,1 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> marks = new ArrayList<>();
+
+        System.out.println("Enter 5 marks:");
+        for (int i = 0; i < 5; i++) {
+            marks.add(sc.nextInt());
+        }
+
+        System.out.println("All Marks: " + marks);
+
+        System.out.println("Highest: " + Collections.max(marks));
+        System.out.println("Lowest: " + Collections.min(marks));
+    }
+}
